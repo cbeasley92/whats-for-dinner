aae253e66f50160198b7cf0855423cb768198f8e
sanksc <sanketchaudhari0608@gmail.com>
2017-04-22 15:50:13 -0500

    Search food based on upc-12,upc-14, name and brandname

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_restful import reqparse, abort, Resource, Api
 from Food import Food
+from name import Name
+from Brand import Brand
 
 from Users import User, Users
 
@@ -17,6 +19,9 @@ api.add_resource(Root, '/')
 api.add_resource(Users, '/api/users')
 api.add_resource(User, '/api/user/<username>')
 api.add_resource(Food, '/api/food/<upccode>')
+api.add_resource(Name, '/api/Name/<itemName>')
+api.add_resource(Brand, '/api/brand/<brandname>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
