0ec6334a35d56bcc18cae7fd6dcbb6f13afaee44
Clinton Beasley <cbeasley92@gmail.com>
2017-04-23 16:00:51 -0500

    Refactoring food lookup

--- Food.py
+++ Food.py
@@ -10,28 +10,78 @@ client = MongoClient('mongodb://cbeasley92:1sL#ak23@cluster0-shard-00-00-klqia.m
                      '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 
 
+class Foods(Resource):
+    def __init__(self):
+        self.db = client.wfd
+
+    def get(self):
+        return json.dumps(self.db.food.find()[0])
+
+
 class Food(Resource):
     def __init__(self):
         self.db = client.wfd
 
-    def get(self, upccode):
-        if len(upccode) == 14:
-            resp = json.dumps(self.db.food.find({'upc14': upccode})[0])
-        elif len(upccode) == 12:
-            resp = json.dumps(self.db.food.find({'upc12': upccode})[0])
+    def get(self, upc):
+        if len(upc) == 14:
+            resp = json.dumps(self.db.food.find({'upc14': upc})[0])
+        elif len(upc) == 12:
+            resp = json.dumps(self.db.food.find({'upc12': upc})[0])
         else:
             resp = json.dumps({})
 
         print resp
         return resp
 
-    def post(self, upccode):
+    def post(self, upc):
         data = request.get_json(force=True)
-        self.db.food.replace_one({'upc14': upccode}, data, upsert=True)
+        self.db.food.replace_one({'upc14': upc}, data, upsert=True)
         print 'Data: %s' % data
         return json.dumps({"results": list(self.db.food.find(data))})
 
-    def put(self, upccode):  # Testing
+    def put(self, upc):  # Testing
         data = request.get_json(force=True)
         print 'Data: %s' % data
-        self.db.food.update({'upc14':upccode }, data)
+        self.db.food.update({'upc14': upc}, data)
+
+
+class Brand(Resource):
+    def __init__(self):
+        self.db = client.wfd
+
+    def get(self, brand_name):
+        resp = json.dumps(self.db.food.find({'brand': brand_name})[0])
+        print resp
+        return resp
+
+    def post(self, brand_name):
+        data = request.get_json(force=True)
+        self.db.food.replace_one({'brand': brand_name}, data, upsert=True)
+        print 'Data: %s' % data
+        return json.dumps({"results": list(self.db.food.find(data))})
+
+    def put(self, brand_name):  # Testing
+        data = request.get_json(force=True)
+        print 'Data: %s' % data
+        self.db.food.update({'brand': brand_name}, data)
+        
+
+class Name(Resource):
+    def __init__(self):
+        self.db = client.wfd
+
+    def get(self, food_name):
+        resp = json.dumps(self.db.food.find({'name': food_name})[0])
+        print resp
+        return resp
+
+    def post(self, food_name):
+        data = request.get_json(force=True)
+        self.db.food.replace_one({'name': food_name}, data, upsert=True)
+        print 'Data: %s' % data
+        return json.dumps({"results": list(self.db.food.find(data))})
+
+    def put(self, food_name):  # Testing
+        data = request.get_json(force=True)
+        print 'Data: %s' % data
+        self.db.food.update({'name': food_name}, data)
--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, request
 from flask_restful import reqparse, abort, Resource, Api
-from Food import Food
-from name import Name
-from Brand import Brand
 
 from Users import User, Users
+from Food import Foods, Food, Name, Brand
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,9 +16,10 @@ class Root(Resource):
 api.add_resource(Root, '/')
 api.add_resource(Users, '/api/users')
 api.add_resource(User, '/api/user/<username>')
-api.add_resource(Food, '/api/food/<upccode>')
-api.add_resource(Name, '/api/Name/<itemName>')
-api.add_resource(Brand, '/api/brand/<brandname>')
+api.add_resource(Foods, '/api/food')
+api.add_resource(Food, '/api/food/upc/<upc>')
+api.add_resource(Name, '/api/food/name/<item_name>')
+api.add_resource(Brand, '/api/food/brand/<brand_name>')
 
 
 if __name__ == '__main__':
